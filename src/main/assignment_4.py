723a694275f3b8f719194be38b3e4af4342dc237
JohnMichael Kane <123655765+johnmichael-kane@users.noreply.github.com>
2024-04-15 18:20:13 -0400

    Update assignment_4.py

---
+++ src/main/assignment_4.py
@@ -0,0 +1,15 @@
+#Use the data of the first example as the data input for these programs. For example, use the data
+#from Example 1 (PP 451 for the Jacobi Iterative method.
+
+#Jacobi Iterative Method, Alg 7.1 - Page 453
+
+#Guass-Seidel Iterative Method, Alg 7.2 - Page 456
+
+#SOR Method, Alg 7.3 - Page 467
+
+#Iterative Refinement Method, Alg 7.4 - Page 474
+
+def main():
+    i=1
+
+    #call the functions here
